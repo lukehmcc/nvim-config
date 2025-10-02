27dde618298e9d0938b93d477a666d9c9c558f08
covalent <mail@lukehmcc.com>
2025-10-02 15:55:20 -0400

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,6 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.keymap.set("i", "<S-CR>", function()
+  return vim.fn.col(".") <= 1 and "<CR>" or "<CR><C-U>"
+end, { expr = true })
